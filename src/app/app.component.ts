c7751f07809a3e841875e20f288d095059cf7193
Kevin Townson <ktown5422@gmail.com>
2024-08-30 17:33:55 -0500

    add home screen title and buttons, setup routes - KT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'weather-desktop-app';
+
+  constructor(private router: Router, private location: Location) { }
+
+
+  onSettingsClick(): void {
+    // Navigate to the settings page
+    this.router.navigate(['/settings']);
+  }
+
+  onLoginClick(): void {
+    // Navigate to the login page
+    this.router.navigate(['/login']);
+  }
+
+  onBackClick(): void {
+    this.location.back();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'settings', component: SettingsComponent },
+    { path: 'login', component: LoginComponent },
+];
